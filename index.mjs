8a8eeb5d7fc37f03a30d4bbde66b071d4c65d79b
hemant-oyo <hemant.kumar8@oyorooms.com>
2022-12-21 12:57:35 +0530

    init commit

--- index.mjs
+++ index.mjs
@@ -27,61 +27,6 @@ if (!depth) {
 
 const jsonData = []
 
-// // Web Crawling
-// function getData(url, currentDepth = 0) {
-//   if (currentDepth > depth) {
-//     return
-//   }
-//   console.log('fetching url: ', url);
-//   fetch(url).then(res => {
-//     console.log('res.ok:' , res.ok);
-//     if (!res.ok) {
-//       console.log(`an error occurred while fetching url: ${url} and the response is: `, res);
-//       return
-//     }
-
-//     res.text().then(htmlString => {
-//       console.log('inside then');
-//       const $ = cheerio.load(htmlString);
-//       const images = $("img");
-//       for (let image of images) {
-//         if (image?.attribs?.src) {
-//           jsonData.push({
-//             imageUrl: image?.attribs?.src,
-//             sourceUrl: url,
-//             depth: currentDepth
-//           })
-//         }
-//       }
-
-//       // This is taking all the piutures
-//       const pictures = $("picture source");
-//       for (let picture of pictures) {
-//         if (picture?.attribs?.srcset) {
-//           jsonData.push({
-//             pictureUrl: picture?.attribs?.srcset,
-//             sourceUrl: url,
-//             depth: currentDepth
-//           })
-//         }
-//       }
-
-//       const anchors = $("a");
-
-//       for (let anchor of anchors) {
-//         const href = anchor?.attribs?.href;
-//         if (href && href !== "/" && href[0] !== "#") {
-//           if (href[0] == "/") {
-//             const baseURL = new URL(url).href;
-//             getData(baseURL + href.substring(1, href.length), ++currentDepth)
-//           }
-//           getData(href, ++currentDepth)
-//         }
-//       }
-//     })
-//   })
-// }
-
 // Web Crawling
 function getData(url, currentDepth = 0) {
   return new Promise(function(resolve, reject) {
